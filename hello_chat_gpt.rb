62cf3221da8ab982561659e816d27465651991b0
JerryFans <fanjiarong_haohao@163.com>
2023-03-06 22:32:07 +0800

    rb config

--- hello_chat_gpt.rb
+++ hello_chat_gpt.rb
@@ -1,40 +1,16 @@
 class HelloChatGpt < Formula
   include Language::Python::Virtualenv
 
-  desc "A chatbot powered by GPT"
+  desc "A program with a command line way to chat with ChatGPT"
   homepage "https://github.com/jerryfans/hello_chat_gpt"
   url "https://github.com/example/hello_chat_gpt/archive/v1.0.0.tar.gz"
-  sha256 "1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
-
-  depends_on "python@3.9"
-
-  resource "requests" do
-    url 
-"https://files.pythonhosted.org/packages/bc/88/1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef/requests-2.26.0-py2.py3-none-any.whl"
-    sha256 "class HelloChatGpt < Formula
-  include Language::Python::Virtualenv
-
-  desc "A chatbot powered by GPT"
-  homepage "https://github.com/jerryfans/hello_chat_gpt"
-  url "https://github.com/example/hello_chat_gpt/archive/v1.0.0.tar.gz"
-  sha256 "1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+  sha256 "1f68d9a63dbfb4ee3d47ccc88bcab9bb68dbb54ba8ff2ae7789ab4d2d86061c4"
 
   depends_on "python@3.9"
 
   resource "requests" do
     url "https://files.pythonhosted.org/packages/d2/f4/274d1dbe96b41cf4e0efb70cbced278ffd61b5c7bb70338b62af94ccb25b/requests-2.28.2-py3-none-any.whl"
     sha256 "64299f4909223da747622c030b781c0d7811e359c37124b4bd368fb8c6518baa"
-  end
-
-  def install
-    virtualenv_install_with_resources
-  end
-
-  test do
-    system bin/"hello_chat_gpt", "--version"
-  end
-end
-"
   end
 
   def install
