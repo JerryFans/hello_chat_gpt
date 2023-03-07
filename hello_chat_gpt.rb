a14c17f0895f5b15634d508f6f93cefe199cadc5
JerryFans <fanjiarong_haohao@163.com>
2023-03-07 20:45:19 +0800

    fix

--- hello_chat_gpt.rb
+++ hello_chat_gpt.rb
@@ -1,12 +1,12 @@
 class HelloChatGpt < Formula
   desc "A command-line tool that generates chat responses using OpenAI's GPT model."
   homepage "https://github.com/JerryFans/hello_chat_gpt"
-  url "https://github.com/JerryFans/hello_chat_gpt/raw/main/release/v1.0/hello_chat_gpt_intel"
-  sha256 "ace62092ac76b7c25caca467ff71e1300b68ba023f8c55f45956eb6dc20a06fc"
+  url "https://github.com/JerryFans/hello_chat_gpt/raw/main/release/v1.0/hello_chat_gpt_intel_v1.0.tar.gz"
+  sha256 "f8c88999e7413bc6218e346a81b8c6f19b314f2c63940579746d8d9f55d0d828"
 
   if Hardware::CPU.arm?
-    url "https://github.com/JerryFans/hello_chat_gpt/raw/main/release/v1.0/hello_chat_gpt_m1"
-    sha256 "5befc858818e46afda39edcee30d70c96d339bbf2ef73d83fb16b0e91747bb6d"
+    url "https://github.com/JerryFans/hello_chat_gpt/raw/main/release/v1.0/hello_chat_gpt_m1_v1.0.tar.gz"
+    sha256 "4ee3e3092ca573957307de9883ae22f186e540460c7843bce6fd5ca3088343c6"
   end
 
   def install
